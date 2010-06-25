be9cae39da2637f736659bed3cc016dfc42a7cc0
Chris Kirkham <chrisjameskirkham@gmail.com>
2010-06-25 13:05:51 +0100

    Re-organised and added some explanatory comments to config.inc.php.
    It is now a little clearer what everything does.

--- config.inc.php
+++ config.inc.php
@@ -1,28 +1,33 @@
 <?php
 
-/*
-//{enable, server default} error reporting
+/*//{enable, server default} error reporting
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-*/
 
-//extract the current directory
-define('ROOT_DIR',		dirname(__FILE__));
 
-//customisable defines
+/* ====== CUSTOMISABLE DEFINES ======
+ * Edit below to adapt to your system
+ * ==================================
+ */
+
+/* The directory 'Smarty.class.php' can be found in */
 define('SMARTY_DIR', 		'/usr/share/php/Smarty/');
-define('TEMPLATE_DIR', 		ROOT_DIR . '/templates');
-define('COMPILED_TEMPLATE_DIR', ROOT_DIR . '/templates_compiled');
-define('CACHE_DIR',		ROOT_DIR . '/cache');
-define('CONTENT_DIR', 		ROOT_DIR . '/content');
 
+/* The root of the website when hosted (Where you would navigate to
+ * to find index.php) */
 define('ROOT_URI', 		'http://localhost/~chris/srweb/');
 
+/* The allowed file extensions. Anything where no parsing is invoved,
+ * can just be added. However, if parsing is required, then a smarty
+ * plugin will need to be written. (But don't worry, it's very easy.) */
 $ALLOWED_TYPES = array(
 	'md',
 	'html'
 );
 
+/* The pages that you wish to appear in the menu should be listed here.
+ * Sub-directories need not be explicitly stated. By including a file
+ * below, its inclusion in the menu is implied. */
 $MENU_PAGES = array(
 	'home', 
 	'test',
@@ -31,5 +36,21 @@ $MENU_PAGES = array(
 	'dir/tester2',
 	'a/very/long/content/path'
 );
+
+
+
+/* ===================================
+ * IF YOU ENJOY MILD PERIL, EDIT BELOW
+ *    Dont say I didn't warn you!
+ * ===================================
+ */
+//extract the current directory
+define('ROOT_DIR',		dirname(__FILE__));
+define('TEMPLATE_DIR', 		ROOT_DIR . '/templates');
+define('COMPILED_TEMPLATE_DIR', ROOT_DIR . '/templates_compiled');
+define('CACHE_DIR',		ROOT_DIR . '/cache');
+define('CONTENT_DIR', 		ROOT_DIR . '/content');
+
+
 
 ?>
