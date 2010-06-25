067419e60ced9c3ab8b9654160b9157c162116ca
Chris Kirkham <chrisjameskirkham@gmail.com>
2010-06-25 15:20:13 +0100

    Added explanatory comments to everything in Menu.class.php.

--- classes/Menu.class.php
+++ classes/Menu.class.php
@@ -1,18 +1,33 @@
 <?php
-
+/*
+ * A class to represent a complete menu. It is dependent upon
+ * MenuItem.class.php as this instantiated class's root is a
+ * MenuItem. This class is used by the smarty plugin 'makemenu',
+ * found under 'plugins/' as 'function.makemenu.php', which
+ * calls its 'getMenuHtml()' method to get the menu.
+ */
 class Menu {
 
+	/*
+	 * Constructor. Instantiates the root MenuItem.
+	 */
 	function __construct(){
 
 		$this->root = new MenuItem(NULL, NULL, NULL);
 
-	}
+	}//__construct
 
 
+
+	/*
+	 * Returns a string containing the un-ordered list of the menu.
+	 * Ensure menu is populated first (addToHierachy(...)).
+	 */
 	function getMenuHtml(){
 
 		$output = "<ul>\n";
 
+		//starting with the root, traverse the tree, concatenating the output.
 		foreach ($this->root->subMenuItems as $item){
 
 			$output .= $item->getItemHtml($this->root_uri);
@@ -27,25 +42,37 @@ class Menu {
 
 
 
+	/*
+	 * Adds a path (such as 'dir/file') to the hierachy. $path is
+	 * the path to the file, relative to the content dir, and
+	 * excluding any extension. $root_uri is the URI that would be
+	 * used to access the directory containing index.php.
+	 */
 	function addToHierachy($path, $root_uri){
 		
+		//get all parts of the path (e.g. 'dir/dir2/file' => array(dir, dir2, file))
 		preg_match_all( '/([a-zA-Z0-9\-\.]+)\/?/' , $path, $matches);
 		$matches = $matches[1];
 
+		//start at the root
 		$previous_item = $this->root;
 
 		$path_tmp = $root_uri;
+
 		foreach($matches as $name){
-			//echo($name.'__');
+
+			//if the menuitem doesn't exist, add it
 			if ($previous_item->getSubMenuItemByName($name) == NULL){
 				$previous_item->addSubMenuItem(new MenuItem($name, $name, $path_tmp.$name));
 			}
+
+			//append the current part of the path to the previous, and update $previous_item
 			$path_tmp .= $name . '/';
 			$previous_item = $previous_item->getSubMenuItemByName($name);
 
 		}//foreach
 
-	}
+	}//addToHierachy
 
 }//class
 
