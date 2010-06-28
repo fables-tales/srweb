b14770ef388c65f59c0aed4386e287ffe0b039b6
Chris Kirkham <chrisjameskirkham@gmail.com>
2010-06-28 18:54:19 +0100

    Added user interogation over the use of config.inc.php; and tidy up.
    User questioned on whether they've edited the config file and opens
    it for them, in their $EDITOR editor of choice, so that they can do
    so if not.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+clear
 echo "Adding write permissions for apache in the following directories:"
 
 echo "  - 'templates_compiled'"
@@ -7,5 +8,33 @@ setfacl -m u:apache:rwx templates_compiled/
 
 echo "  - 'cache'"
 setfacl -m u:apache:rwx cache/
+
+echo ""
+echo -e "Have you checked the settings in 'config.inc.php'? [Y/n]: \c"
+read input
+
+if [[ $input == "n" ]]
+then
+	echo ""
+	echo "You should!"
+	echo "The most important thing is the ROOT_URI setting."
+	echo "Perhaps, one day, this script will do that for you."
+	echo ""
+
+	if [[ $EDITOR == "" ]]
+	then
+		EDITOR=vi
+	fi
+
+	echo -e "\nEdit in $EDITOR now? [Y/n]: \c"
+	read input2
+
+	if [[ ! $input2 == "n" ]]
+	then
+		$EDITOR config.inc.php
+	fi
+else
+	echo "Well done!"
+fi
 
 exit 0
