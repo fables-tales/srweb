7005e6537405f609eb7d890473ca03f3e6548c48
Chris Kirkham <chrisjameskirkham@gmail.com>
2010-06-28 16:11:35 +0100

    Replaced some of $MENU_PAGES's items in config.inc.php with their associative counterparts and added some extra explanatior comments.
    New associative mappings now exist with each page's title/text mapped
    to its path. The additional comments explain how to use the menu system.

--- config.inc.php
+++ config.inc.php
@@ -26,16 +26,40 @@ $ALLOWED_TYPES = array(
 );
 
 /* The pages that you wish to appear in the menu should be listed here.
- * Sub-directories need not be explicitly stated. By including a file
- * below, its inclusion in the menu is implied. */
+ * There are two way to add a page to the menu:
+ *
+ *	1) simply add its path to the list. All parent directories
+ * 	   will be included in the hierachy, and if not named before,
+ * 	   will fall back to the page name (excl. ext.)
+ *
+ *	2) add its desired name mapped to its path to the list. i.e.
+ *
+ *	     'DIR NAME'  => 'dir/',
+ *	     'SOME FILE' => 'dir/afile'
+ *
+ *	   will produce a hierachy (in text) as:
+ *
+ *	      DIR NAME
+ *	      - SOME FILE
+ *
+ * Please note, however, that once a path has been given a nice title,
+ * it cannot be changed. When adding menu items, work hierachically:
+ * it makes it easier to read, and is also necessary to get all titles
+ * to display correctly.
+ *
+ * Please also note that to name a directory, it needs a trailing slash.
+ */
 $MENU_PAGES = array(
-	'home', 
-	'test',
-	'dir',
-	'dir/tester',
-	'dir/tester2',
-	'a/very/long/content/path',
-	'mission'
+//	TITLE/NAME		=>  PATH
+//     =====================================================
+	'Home' 			=> 'home',
+	'Documentation \'09' 	=> 'dir/',
+	'Motor Board Docs' 	=> 'dir/tester',
+	'TESTER 2' 		=> 'dir/tester2/',
+	'Another File' 		=> 'dir/tester2/anotherfile',
+	'Mission Statement' 	=> 'mission',
+	'Sponsors' 		=> 'sponsors',
+	'Contact Us' 		=> 'contactus'
 );
 
 
