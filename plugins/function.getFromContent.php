b162ac83813b6a6d541e85b1446e915aa48fafa7
Chris Kirkham <chrisjameskirkham@gmail.com>
2010-06-30 22:03:31 +0100

    Added some explanatory comments to the 'getFromContent' smarty plugin.

--- plugins/function.getFromContent.php
+++ plugins/function.getFromContent.php
@@ -11,11 +11,15 @@
 function smarty_function_getFromContent($params, &$smarty)
 {
 
+	//grab Content object (assigned to smarty)
 	$c = $smarty->get_template_vars("content");
+
+	//if the request is for CONTENT, get the parsed content
 	if (strtoupper($params['get']) == "CONTENT"){
 
 		return $c->getParsedContent();
 
+	//if it's not, assume it's a metadata field from the content file. 
 	} else {
 
 		return $c->getMeta(strtoupper($params['get']));
