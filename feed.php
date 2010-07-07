bb245dc9107f80b965dbd5b41b1fc06da89fb165
Chris Kirkham <chrisjameskirkham@gmail.com>
2010-07-07 13:58:00 +0100

    Created feed.php, which outputs the RSS feed, but uses caching.
    This is effectively the same code as ./createfeed, but it has some
    caching wrapped around it. It uses memcache.

---
+++ feed.php
@@ -0,0 +1,107 @@
+<?php
+
+define('MEMCACHE_SERVER', 	'127.0.0.1');
+define('MEMCACHE_PORT',		'11211');
+
+$feed = NULL;
+
+if (extension_loaded('memcache')){
+
+	$memcache = new Memcache();
+	if($memcache->connect(MEMCACHE_SERVER, MEMCACHE_PORT)){
+
+		if (!($feed = $memcache->get('feed_content'))){
+			$feed = getFeedContent();
+			$memcache->set('feed_content', $feed, 0, 30);
+		}
+
+	}//if connect
+
+}//if extension_loaded
+
+if ($feed === NULL)
+	$feed = getFeedContent();
+
+if ($feed !== NULL){
+
+	Header("Content-Type: application/xml; charset=ISO-8859-1");
+	echo $feed;
+
+} else {
+
+	Header("HTTP/1.1 500 Internal Server Error");
+
+}
+
+
+
+
+
+
+
+
+
+function getFeedContent(){
+
+	require('config.inc.php');
+	require('classes/Content.class.php');
+
+	$BASE_URL = !empty($_SERVER['HTTPS'])
+		? 'https://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/'
+		: 'http://'  . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/';
+
+	$newsItems = array();
+
+	if ($d = opendir(CONTENT_DIR . '/news')){
+
+		while (false !== ($file = readdir($d))){
+
+			if (substr($file, 0, 1) != "."){
+
+				$newsItems[] = new Content(CONTENT_DIR . '/news/' . $file);
+
+			}//if
+
+		}//while
+
+		closedir($d);
+
+		if (count($newsItems) > 0){
+
+			$output  = "<?xml version='1.0'?><rss version='2.0'><channel>";
+			$output .= "<title>Student Robotics Latest News</title>";
+			$output .= "<link>" . $BASE_URL . "news/</link>";
+			$output .= "<lastBuildDate>" . date(DATE_RSS) . "</lastBuildDate>";
+			$output .= "<description>All the latest news from Student Robotics</description>";
+
+			usort($newsItems, "dateStrCmp");
+
+			foreach ($newsItems as $newsItem){
+
+				$output .= "<item>";
+
+				$output .= "<title><![CDATA[" . $newsItem->getMeta('TITLE') . "]]></title>";
+				$output .= "<link>" . $BASE_URL . str_replace(CONTENT_DIR . '/', '', $newsItem->filename) . "</link>";
+				$output .= "<guid>" . $BASE_URL . str_replace(CONTENT_DIR . '/', '', $newsItem->filename) . "</guid>";
+				$output .= "<description><![CDATA[" . $newsItem->getMeta('DESCRIPTION') . "]]></description>";
+				$output .= "<pubDate>" . date(DATE_RSS, strtotime($newsItem->getPubDate())) . "</pubDate>";
+
+				$output .= "</item>";
+
+			}//foreach
+
+			$output .= "</channel></rss>";
+
+			return $output;
+
+		}
+ 
+	} 
+
+	return NULL;
+
+}//getFeedContent
+
+
+
+?>
