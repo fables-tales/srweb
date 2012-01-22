25de9b92d1f500c8618b2c7aac7d3a2161938477
Peter Law <PeterJCLaw@gmail.com>
2012-01-22 01:20:22 +0000

    Populate the team-college LUT with data from trac.
    This information is lifted directly from the 2012/Schools trac page:
    https://www.studentrobotics.org/trac/wiki/2012/Schools. Any corrections
    should therefore probably be applied in both places.
    
    Note that this is currently assuming that the LUT needs to use the
    team's ids rather than numbers. In the event that this is wrong,
    the teams are in the same order as the trac page, and thus also in
    numeric order.

--- classes/team_info_college_lut.php
+++ classes/team_info_college_lut.php
@@ -1,7 +1,53 @@
 <?php
 
+$_ARG = array('URL' => 'http://www.ac-grenoble.fr/argouges/',		'name' => 'Lycée Argouges');
+$_BWS = array('URL' => 'http://www.bws.wilts.sch.uk/',				'name' => 'Bishop Wordsworth School');
+$_BGR = array('URL' => 'http://www.bristolgrammarschool.co.uk/',	'name' => 'Bristol Grammar School');
+$_BRK = array('URL' => 'http://www.brock.ac.uk/',					'name' => 'Brockenhurst College');
+$_BSG = array('URL' => 'http://www.bsg.bournemouth.sch.uk/',		'name' => 'Bournemouth School for Girls');
+$_CLF = array('URL' => 'http://www.cliftonhigh.bristol.sch.uk/',	'name' => 'Clifton High School');
+$_EMM = array('URL' => 'http://www.ac-grenoble.fr/mounier/',		'name' => 'Lycée Emmanuel Mounier');
+$_GRS = array('URL' => 'http://www.greshams.com/',					'name' => 'Gresham\'s');
+$_GMR = array('URL' => 'http://www.farn-ct.ac.uk/',					'name' => 'Farnborough');
+$_HRS = array('URL' => 'http://www.hrsfc.ac.uk/',					'name' => 'Hills Road Sixth Form College');
+$_HZW = array('URL' => 'http://www.hazelwick.w-sussex.sch.uk/',		'name' => 'Hazelwick Comprehensive School');
+$_MFG = array('URL' => 'http://www.themfg.co.uk/',					'name' => 'Mirfield Free Grammar');
+$_PSC = array('URL' => 'http://www.psc.ac.uk/',						'name' => 'Peter Symonds');
+$_QEH = array('URL' => 'http://www.qehbristol.co.uk/',				'name' => 'Queen Elizabeth\'s Hospital School Bristol');
+$_QMC = array('URL' => 'http://www.qmc.ac.uk/',						'name' => 'Queen Mary\'s College');
+$_RMS = array('URL' => 'http://www.redmaids.bristol.sch.uk/',		'name' => 'Red Maids Bristol');
+$_SEN = array('URL' => 'http://www.shsb.org.uk/',					'name' => 'Southend School');
+$_STA = array('URL' => 'http://www.st-annes.southampton.sch.uk/',	'name' => 'St. Anne\'s');
+$_TTN = array('URL' => 'http://www.tauntons.ac.uk/',				'name' => 'Tauntons College');
+
+$_MUC = array('name' => 'Munich');
+
 $team_info_college_lut = array(
 	'SRZ' => array('name' => "School of SR", 'URL' => "http://www.bbc.co.uk/"),
+	'ARG'  => $_ARG,
+	'BWS'  => $_BWS,
+	'BWS2' => $_BWS,
+	'BGR'  => $_BGR,
+	'BRK'  => $_BRK,
+	'BSG'  => $_BSG,
+	'CLF'  => $_CLF,
+	'CLF2' => $_CLF,
+	'EMM'  => $_EMM,
+	'GRS'  => $_GRS,
+	'GMR'  => $_GMR,
+	'HRS'  => $_HRS,
+	'HZW'  => $_HZW,
+	'MFG'  => $_MFG,
+	'PSC'  => $_PSC,
+	'PSC2' => $_PSC,
+	'QEH'  => $_QEH,
+	'QMC'  => $_QMC,
+	'RMS'  => $_RMS,
+	'SEN'  => $_SEN,
+	'SEN2' => $_SEN,
+	'STA'  => $_STA,
+	'TTN'  => $_TTN,
+	'MUC'  => $_MUC,
 );
 
 ?>
